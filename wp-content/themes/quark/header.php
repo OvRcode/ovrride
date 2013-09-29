fc24e03e8f1431970cb83dd5195c93a03468d6ff
AJ Acevedo <aj@ajalabs.com>
2013-09-28 19:08:48 -0600

    Chapman's fix for issue #32

--- wp-content/themes/quark/header.php
+++ wp-content/themes/quark/header.php
@@ -27,7 +27,7 @@
 	<!-- Responsive and mobile friendly stuff -->
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="device-width; minimum-scale=1.0; initial-scale=1.0; maximum-scale=1.0;"/>
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
