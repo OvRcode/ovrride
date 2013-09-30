4c458b6fb8b9fecef52911af0e973667ea9069a2
AJ Acevedo <aj@ajalabs.com>
2013-09-30 19:58:09 -0400

    Oops I broke it, temp fix

--- lists/include/config.php
+++ lists/include/config.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * OvR Lists - The base configurations
+ *
+ * @package OvR Lists
+ * @since Version 0.0.2
+ */
+
 # MySQL Database Connection
 $host = 'localhost';
 $user = '***REMOVED***';
