8a077b47ccfd5638ecf72a35b9df84e0c1ed6cde
AJ Acevedo <aj@ajalabs.com>
2013-09-30 16:10:04 -0400

    Refactored out includes

--- lists/include/config.php
+++ lists/include/config.php
@@ -1,3 +1,7 @@
 <?php
-// Placeholder for configuration includes
+# MySQL Database Connection
+$host = 'localhost';
+$user = 'ovrridec_ovrride';
+$pass = 'PmInUbYv12BuNiMo';
+$db = 'ovrridec_ovrride';
 ?>
