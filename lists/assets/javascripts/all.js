db8b1ea08a66744619e52ae977d1e7ca4c0ce45e
AJ Acevedo <aj@ajalabs.com>
2013-10-04 22:35:25 -0400

    Bootstrap and Grunt locked and loaded
    - Extracted Check All javascript to lists.js
    - Updated README with Grunt details
    - grunt watch now watches sass, and js files

--- lists/assets/javascripts/all.js
+++ lists/assets/javascripts/all.js
@@ -11786,4 +11786,19 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
   })
 
 }(window.jQuery);
-;
+;/**
+*  OvR Lists - Custom JavaScript
+*
+*/
+
+// Order Status: Check All / Uncheck All
+function checkAll(formname, checktoggle) {
+  var checkboxes = [];
+  checkboxes = document[formname].getElementsByTagName('input');
+
+  for (var i=0; i<checkboxes.length; i++) {
+    if (checkboxes[i].type == 'checkbox') {
+      checkboxes[i].checked = checktoggle;
+    }
+  }
+}
