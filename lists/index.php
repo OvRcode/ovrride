1092e9d96f8369f76bc8bd4b17551a8f457cedda
AJ Acevedo <aj@ajalabs.com>
2013-10-06 01:41:56 -0400

    Stylin

--- lists/index.php
+++ lists/index.php
@@ -22,6 +22,7 @@ else
   $list = new Trip_List("");
 ?>
 <!DOCTYPE html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <title>OvR Trip Lists</title>
@@ -33,29 +34,67 @@ else
   <body>
     <h1>OvR Trip Lists</h1>
     <br>
+
     <form action="index.php" method="post" name="trip_list">
+      <section class="trip-select">
       <label>Select a Trip:</label>
       <br>
       <select id="trip" name="trip">
       <?php echo $list->select_options; ?>
       </select>
+      </section>
       <br>
+
+      <section class="order-status-select">
       <label>Order Status: </label>
-      <a onclick="javascript:checkAll('trip_list', true);" href="javascript:void();">Check All</a> /
-      <a onclick="javascript:checkAll('trip_list', false);" href="javascript:void();">Uncheck All</a><br />
-      <input type="checkbox" name="processing" value="processing" <?php if(isset($_POST['processing']) || !isset($_POST['trip'])) echo 'checked';?>>Processing</input>
-      <input type="checkbox" name="pending" value="pending" <?php if(isset($_POST['pending']) || !isset($_POST['trip'])) echo 'checked'; ?>>Pending</input>
-      <input type="checkbox" name="cancelled" value="cancelled" <?php if(isset($_POST['cancelled'])) echo 'checked'; ?>>Cancelled</input>
-      <input type="checkbox" name="failed" value="failed" <?php if(isset($_POST['failed'])) echo 'checked'; ?>>Failed</input>
-      <input type="checkbox" name="on-hold" value="on-hold" <?php if(isset($_POST['on-hold'])) echo 'checked'; ?>>On-hold</input>
-      <input type="checkbox" name="completed" value="completed" <?php if(isset($_POST['completed'])) echo 'checked'; ?>>Completed</input>
-      <input type="checkbox" name="refunded" value="refunded" <?php if(isset($_POST['refunded'])) echo 'checked'; ?>>Refunded</input>
+      <a onclick="javascript:checkAll('trip_list', true);" href="javascript:void();">Check All</a> &#47;
+      <a onclick="javascript:checkAll('trip_list', false);" href="javascript:void();">Uncheck All</a>
       <br>
-      <input type="submit" value="Generate List" />
+      <label class="checkbox-inline">
+        <input type="checkbox" name="processing" value="processing" <?php if(isset($_POST['processing']) || !isset($_POST['trip'])) echo 'checked';?>>Processing</input>
+      </label>
+      <label class="checkbox-inline">
+        <input type="checkbox" name="pending" value="pending" <?php if(isset($_POST['pending']) || !isset($_POST['trip'])) echo 'checked'; ?>>Pending</input>
+      </label>
+      <label class="checkbox-inline">
+        <input type="checkbox" name="cancelled" value="cancelled" <?php if(isset($_POST['cancelled'])) echo 'checked'; ?>>Cancelled</input>
+      </label>
+      <label class="checkbox-inline">
+        <input type="checkbox" name="failed" value="failed" <?php if(isset($_POST['failed'])) echo 'checked'; ?>>Failed</input>
+      </label>
+      <label class="checkbox-inline">
+        <input type="checkbox" name="on-hold" value="on-hold" <?php if(isset($_POST['on-hold'])) echo 'checked'; ?>>On-hold</input>
+      </label>
+      <label class="checkbox-inline">
+        <input type="checkbox" name="completed" value="completed" <?php if(isset($_POST['completed'])) echo 'checked'; ?>>Completed</input>
+      </label>
+      <label class="checkbox-inline">
+        <input type="checkbox" name="refunded" value="refunded" <?php if(isset($_POST['refunded'])) echo 'checked'; ?>>Refunded</input>
+      </label>
+      <br>
+      <input type="submit" class="generate-list" value="Generate List" />
+      </section>
       </form>
       <br>
 
       <?php if(isset($_POST['trip']) && $_POST['trip'] != "") print $list->html_table; ?>
+
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 text-left">
+              <h5><span>&copy; Copyright <?php echo date('Y'); ?> - <a href="/">OvR ride LLC.</a></span></h5>
+            </div>
+          <div class="col-md-4 text-center footer-center">
+            <h5>For OvR Staff Use Only</h5>
+          </div>
+          <div class="col-md-4 text-right">
+            <h5>Version <?php echo $lists_version ?></h5>
+          </div>
+        </div>
+      </div>
+      </footer>
+
       <!-- Include concatenated and minified javascripts -->
       <script src="assets/javascripts/all.min.js"></script>
 
