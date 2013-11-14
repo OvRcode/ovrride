2fcd1f0236c1abc804b938743abcade704847a5e
Mike Barnard <michael.d.barnard@gmail.com>
2013-11-14 16:04:28 -0500

    new tables
    ovr_lists_checkboxes and ovr_lists_manual_orders will replace the
    previous ovr_lists_table table

---
+++ lists/ovr_lists_checkboxes.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `ovr_lists_checkboxes` (
+  `ID` varchar(32) NOT NULL,
+  `AM` tinyint(1) DEFAULT NULL,
+  `PM` tinyint(1) DEFAULT NULL,
+  `Waiver` tinyint(1) DEFAULT NULL,
+  `Product` tinyint(1) DEFAULT NULL,
+  `Bus` tinyint(1) DEFAULT NULL,
+  `All_Area` tinyint(1) DEFAULT NULL,
+  `Beg` tinyint(1) DEFAULT NULL,
+  `BRD` tinyint(1) DEFAULT NULL,
+  `SKI` tinyint(1) DEFAULT NULL,
+  `LTS` tinyint(1) DEFAULT NULL,
+  `LTR` tinyint(1) DEFAULT NULL,
+  `Prog_Lesson` tinyint(1) DEFAULT NULL,
+  UNIQUE KEY `ID` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
