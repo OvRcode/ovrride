307e90d286352045c5bab75004fcaf8fcf22417d
Mike Barnard <michael.d.barnard@gmail.com>
2014-06-03 14:09:32 -0400

    Fixed Rockaway To/From Fields values were being mangled by checkbox state function in php

--- lists/assets/javascripts/all.js
+++ lists/assets/javascripts/all.js
@@ -13294,6 +13294,8 @@ $('#save').click(function(){
 $.fn.getData = function(){
   var jqxhr = $.post("/pull.php", {'requestType':'orders','trip' : $(this).val()})
   .done(function(data){
+    console.log('PHP DATA:');
+    console.log(data);
     window.orderData = data;
     window.storage.set('orderData',orderData);
     $("#listTable").buildTable();
