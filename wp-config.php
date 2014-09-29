d49376357cc39438665c0d2f4445f12f520b2d42
Mike Barnard <michael.d.barnard@gmail.com>
2014-09-29 16:49:55 -0400

    fixed a syntax error in config

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define('DB_USER', ( getenv('AMAZON_RDS_USER') ?: '***REMOVED***' ) );
 define('DB_PASSWORD', ( getenv('AMAZON_RDS_PASS') ?: '***REMOVED***' ) );
 
 /** MySQL hostname */
-define('DB_HOST' ( getenv('AMAZON_RDS_HOST') ?: 'localhost'));
+define('DB_HOST', ( getenv('AMAZON_RDS_HOST') ?: 'localhost'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
