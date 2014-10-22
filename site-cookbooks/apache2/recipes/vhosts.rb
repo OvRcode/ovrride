29fbd035edda1953f1abd8cd43c7b7545b2a2601
Mike Barnard <michael.d.barnard@gmail.com>
2014-10-22 11:20:50 -0400

    Base server setup -still needs a few config tweaks to get site loading

---
+++ site-cookbooks/apache2/recipes/vhosts.rb
@@ -0,0 +1,13 @@
+# Cookbook Name:: apache2
+# Recipe:: vhosts
+#
+
+include_recipe "apache2"
+
+web_app "ovr" do
+  server_name "local.ovrride.com"
+  server_aliases ["http://local.ovrride.com"]
+  directory_index "index.php"
+  allow_ovrride "all"
+  docroot "/vagrant/"
+end
