f49bc3d4d14db3093d8305440f91a536a5f4b3bf
Mike Barnard <michael.d.barnard@gmail.com>
2014-12-02 09:16:07 -0500

    Added shell of class for plugin

---
+++ wp-content/plugins/woocommerce-customer-note-email/class-wc-customer-note-email.php
@@ -0,0 +1,13 @@
+<?php
+
+if (! defined( 'ABSPATH' ) ) exit; //Exit if accessed directly
+
+/**
+ * A customer Customer Note WooCommerce Email Class
+ *
+ * @since 0.1
+ * @extends \WC_Email
+ */
+class WC_Customer_Note_Email extends WC_Email {
+  
+} // end \WC_Customer_Note_Email class
