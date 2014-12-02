1481379f43757e1cc355818cb43426c8173d5ecb
Mike Barnard <michael.d.barnard@gmail.com>
2014-12-02 08:51:57 -0500

    Fixed length of file names -didn't account for longer day names

--- chef/getDB.sh
+++ chef/getDB.sh
@@ -6,7 +6,7 @@ if [ -f /vagrant/ovrride.sql ]; then
 fi
 echo 'Identify Latest DB Snapshot'
 # s3cmd lists files, egrep searches for correct snapshot, cut drops extra data from s3cmd
-DBFILE=$(s3cmd -c /vagrant/chef/s3cfg ls S3://ovrdatabase/latest/ | egrep '(ovrride.{19}(Mon|Tues|Wednes|Thurs|Fri|Sat|Sun)day.*)' | cut -c54-92)
+DBFILE=$(s3cmd -c /vagrant/chef/s3cfg ls S3://ovrdatabase/latest/ | egrep '(ovrride.{19}(Mon|Tues|Wednes|Thurs|Fri|Sat|Sun)day.*)' | cut -c54-95)
 echo 'Downloading compressed DB'
 s3cmd -c /vagrant/chef/s3cfg get S3://ovrdatabase/latest/$DBFILE /vagrant/ovrride.sql.gz
 #s3cmd -c /vagrant/chef/s3cfg get S3://ovrdatabase/latest/ovrride* /vagrant/ovrride.sql.gz 
