5a780260345128be1ac6f9697cd63b9193630761
Mike Barnard <michael.d.barnard@gmail.com>
2014-12-02 09:01:38 -0500

    Added base for a new custom email plugin

---
+++ wp-content/plugins/woocommerce-customer-note-email/woocommerce-customer-note-email.php
@@ -0,0 +1,14 @@
+<?php
+  /**
+   * Plugin Name: WooCommerce Customer Note Email
+   * TODO:ADD URI
+   * Description: Plugin to add an extra email when a customer adds a note to an order
+   * Author: Mike Barnard
+   * Version: 0.1
+   *
+   * License: GNU General Public License v3.0
+   * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+   *
+   */
+  
+  if ( ! defined( 'ABSPATH' ) ) exit; //Exit if accessed directly
