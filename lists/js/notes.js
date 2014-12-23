cc0916ef72d4e34f4b1530f6da46b9c1c251d19a
Mike Barnard <michael.d.barnard@gmail.com>
2014-12-23 09:27:33 -0500

    Added bus field to notes

--- lists/js/notes.js
+++ lists/js/notes.js
@@ -1,12 +1,32 @@
+// Number padding for timestamp generation
+Number.prototype.pad = function(size) {
+      var s = String(this);
+      while (s.length < (size || 2)) {s = "0" + s;}
+      return s;
+    }
+$(function(){
+    //add check for online/offline when offline is implemented
+    //getNotes();
+});
+function timestamp(){
+    var date    = new Date();
+    var year    = date.getFullYear();
+    var month   = date.getMonth().pad();
+    var day     = date.getDay().pad();
+    var hours   = date.getHours().pad();
+    var minutes = date.getMinutes().pad();
+    var seconds = date.getSeconds().pad();
+
+    return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+}
 function outputNotes(){
     if (! jQuery.isEmptyObject(notes.keys()) ){
-        var output = "<h3>Notes</h3>";
+        var output = "";
         var allNotes = notes.keys();
         jQuery.each(allNotes, function(key, value){
             var note = notes.get(value);
             output = output.concat("<p>" + value + ": " + note + "</p>");
         });
-        $("#content").append(output);
-    }
-    
+        $("#notesContent").append(output);
+    }   
 }
