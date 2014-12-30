36a0979f1e5ca0220aa0b3e701974da4f10f1741
Mike Barnard <michael.d.barnard@gmail.com>
2014-12-30 10:14:26 -0500

    Destination add/remove works

--- lists/js/admin.js
+++ lists/js/admin.js
@@ -1,10 +1,35 @@
 $(function(){
+    // Enable popovers
+    $("[data-toggle=popover]").popover();
+    $('[data-toggle="popover"]').on('shown.bs.popover', function(){
+        checkDestPopover();
+        $("#destName").on("keyup", function(){
+            checkDestPopover();
+        });
+        $("#addDestBtn").on("click",function(){ saveDestination() });
+    });
+    $('[data-toggle="popover"]').on('hide.bs.modal', function(){
+        $("#destName").unbind("keyup");
+        $("#addDestBtn").unbind("click");
+    });
     getDestinations();
 });
+function saveDestination(){
+    $.post("api/dropdown/destination/update", { destination: $("#destName").val(), enabled: "Y"});
+    location.reload();
+}
+function checkDestPopover(){
+    var destName = $("#destName").val();
+    if (  destName == "" ) {
+        $("#addDestBtn").addClass('disabled');
+    } else if ( $("#addDestBtn").hasClass('disabled') ) {
+        $("#addDestBtn").removeClass('disabled');
+    }
+}
 function getDestinations(){
     $.getJSON( "api/dropdown/destination/all", function(data){
         window.destOutput = "<table id='destTable'>\
-                <thead><tr><th></th><th>Enabled</th><th>Disabled</th></tr></thead>\
+                <thead><tr><th></th><th>Enabled</th><th>Disabled</th><th>Remove</th></tr></thead>\
                 <tbody>";
         jQuery.each(data, function(key,value){
             //console.log("Key: " + key + " Value: " + value);
@@ -19,6 +44,7 @@ function getDestinations(){
             } else {
                 row = row.concat('<td><input type="radio" value="N" name="' + key + '"></td>');
             }
+            row = row.concat('<td><input type="radio" value="Delete" name="' + key + '"></td>');
             destOutput = destOutput.concat(row);
         });
         var foot = "</tbody>\
@@ -43,4 +69,5 @@ function updateDestinations(){
         var radioValue = $("tr[class='" + thisClass + "'] input:radio:checked").val();
         $.post("api/dropdown/destination/update", { destination: thisClass, enabled: radioValue});
     });
+    location.reload();
 }
