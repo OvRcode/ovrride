a064fcd77fb0e0e7dea61ccb48b3955d56d8bbb7
Mike Barnard <michael.d.barnard@gmail.com>
2015-01-01 11:00:36 -0500

    Fixed summary when all fields are not present

--- lists/js/uncompressed/summary.js
+++ lists/js/uncompressed/summary.js
@@ -2420,14 +2420,14 @@ if (typeof jQuery === 'undefined') {
             toggleMenuButtons("online");
             if ( statusIcon.hasClass('btn-danger') ) {
                 statusIcon.removeClass('btn-danger')
-                    .addClass('btn-success')
+                    .addClass('btn-black')
                     .html('<i class="fa fa-signal"></i> Online');
             }
         } else if (!window.navigator.onLine) {
             toggleMenuButtons("offline");
             
-            if ( statusIcon.hasClass('btn-success') ) {
-                statusIcon.removeClass('btn-success')
+            if ( statusIcon.hasClass('btn-black') ) {
+                statusIcon.removeClass('btn-black')
                     .addClass('btn-danger')
                     .html('<i class="fa fa-plane"></i> Offline');
             }
@@ -2485,8 +2485,10 @@ function getTripData(){
                     orders.set(id,value);
                     if ( 'Pickup' in value )
                         settings.set('Pickup', 1);
-                } else {
+                } else if ( key == 'HTML' ){
                     initialHTML.set(id,value);
+                } else if ( key == 'State' && value !== null) {
+                  tripData.set(id+":"+value, 1);
                 }
             });
         });
@@ -2527,11 +2529,16 @@ function parseData(){
                }
                // Add to pickup location
                pickups[pickup].Expected++;
-               if ( tripData.isSet(value + ":AM" ) ) {
+               if ( tripData.isSet(value + ":AM") ||
+               ( ! tripData.isSet(value + ":AM") && tripData.isSet(value + ":Waiver") ) ||
+               ( ! tripData.isSet(value + ":AM") && tripData.isSet(value + ":Product") ) ) {
                    pickups[pickup].AM++;
                }
                if ( tripData.isSet(value + ":PM" ) ) {
                    pickups[pickup].PM++;
+                   if ( ! tripData.isSet(value + ":AM") && ! tripData.isSet(value + ":Waiver") && ! tripData.isSet(value + ":Product")) {
+                     pickups[pickup].AM++;
+                   }
                }
         }
         if ( tripData.isSet(value+":AM") )
