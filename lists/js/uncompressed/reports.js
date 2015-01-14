f856535830a1c7cdcfb8baef26543d39b88fc41c
Mike Barnard <michael.d.barnard@gmail.com>
2015-01-14 15:01:19 -0500

    Fixed manifest path, generated new js files

--- lists/js/uncompressed/reports.js
+++ lists/js/uncompressed/reports.js
@@ -2565,103 +2565,69 @@ Number.prototype.pad = function(size) {
   while (s.length < (size || 2)) {s = "0" + s;}
   return s;
 };;$(function(){
-    if ( ! window.navigator.onLine && ! jQuery.isEmptyObject(orders.keys())) {
-      window.location.href = "list.php";
-    } else if ( ! window.navigator.onLine && jQuery.isEmptyObject(orders.keys())) {
-      alert ("No Local Data found, cannot download trips while offline");
-    }
-    /* Start Drop Down population */
-    if ( ! window.navigator.onLine ) {
-          window.location.href = "list.php";
+  if ( jQuery.browser.mobile && navigator.userAgent.match(/iPad/i) === null ){
+    $("span.mobileButtons").removeClass('hidden');
+  }
+    //add check for online/offline when offline is implemented
+    refreshReports();
+    $("#saveReport").click(function(){ saveReport(); });
+    $("#refreshReports").click(function(){ refreshReports(); });
+    $("#bus").click(function(){ toggleBus(); });
+});
+function toggleBus(){
+    var bus = settings.get('bus');
+    var selector = "div#reportsContent :not(p:contains('Bus " + bus +"'))";
+    var buttonValue = $("#bus").val();
+    if ( buttonValue == "show" ) {
+        $(selector).hide();
+        $("#bus").val('hide');
+        $("#bus").html('<i class="fa fa-bus"></i>&nbsp;Show All Buses');
     } else {
-      $.get("api/dropdown/destination", function(data, dd){
-        $('#destination').append(data); 
-      })
-      .done(function(data){
-        window.dd.set('destination', data);
-        // Download trips data if destinations load
-        tripDropdown();
-      })
-      .fail(function(){
-        alert('Destination data failed to load, please refresh page');
-      }); 
-    }
-    // All Checkboxes on settings page w/selectors
-    window.checkBoxes = {
-        "balance":    $("#balance"),
-        "cancelled":  $("#cancelled"),
-        "completed":  $("#completed"),
-        "failed":     $("#failed"),
-        "finalized":  $("#finalized"),
-        "no-show":    $("#no-show"),
-        "on-hold":    $("#on-hold"),
-        "processing": $("#processing"),
-        "pending":    $("#pending"),
-        "refunded":   $("#refunded"),
-        "walk-on":    $("#walk-on")
-    };
-    $('#default').click(function(){ resetStatuses("Default"); });
-    $('#clear').click(function(){ resetStatuses("All"); });
-    $('#generate_list').click(function(){
-        saveOptions();
-        getTripData();
-    });
-}); 
-
-function tripDropdown(){
-    $.get("api/dropdown/trip", function(data){
-            $('#trip').append(data); 
-    })
-    .done(function(data){
-        $('#trip').chained("#destination");
-        window.dd.set('trip',data);
-        checkSettings();
-    })
-    .fail(function(){
-        alert('Trip dropdown failed to load, please refresh page');
-    });
-}
-function checkSettings(){
-    if ( settings.isSet('destination') ) {
-        $('#destination').val(settings.get('destination')).trigger('change');
-        if ( settings.isSet('tripNum') ) {
-            $('#trip').val(settings.get('tripNum'));
-        }
-    }
-    if ( settings.isSet('status') ) {
-        var status = settings.get('status');
-        var statusList = status.split(",");
-        jQuery.each(statusList, function(index,value){
-            $("input:checkbox[value="+value+"]").prop('checked','checked');
-        });
-    }
-    if ( settings.isSet('bus') ) {
-        $('#bus').val(settings.get('bus'));
+        $(selector).show();
+        $("#bus").val("show");
+        $("#bus").html('<i class="fa fa-bus"></i>&nbsp;This Bus Only');
     }
 }
-function saveOptions(){
-    settings.removeAll();
-    window.settings.set('destination', $('#destination').val());
-    var trip = $('#trip');
-    window.settings.set('tripNum', $('#trip').val());
-    window.settings.set('tripName', $('#trip option:selected').text());
-    window.settings.set('bus', $('#bus').val());
+function saveReport(){
+    var report = $("#newReport").val();
+    var bus = settings.get('bus');
+    var trip = settings.get('tripNum');
+    var timestamp = timeStamp();
     
-    // Array to be filled with active checkbox values then concatenated with , seperator
-    window.activeBoxes = [];
-    jQuery.each(window.checkBoxes, function(index, value){
-        if ( value.prop('checked') ){
-            window.activeBoxes[window.activeBoxes.length] = value.val();
-        }
-    });
-    window.settings.set('status', window.activeBoxes.join());
+    if ( window.navigator.onLine ){
+      onlineReportSave(report,bus,trip);
+    } else {
+      reports.set(timestamp, "Bus " + bus + ": " + report);
+      unsavedReports.set(timestamp,1);
+    }
+    /*jshint -W030 */ 
+    $("#reportsContent").append("<p>" + timestamp + ": Bus " + settings.get('bus') + ": " + $("#newReport").val()).after() + "</p>";
+    $("#newReport").val('');
 }
-function resetStatuses(type){
-    jQuery.each(window.checkBoxes, function(index,value){
-        if ( type == 'All' || (type == 'Default' && (index != 'completed' && index != 'processing' && index != 'walk-on'))){
-            value.prop('checked','');
-        } else if ( type == 'Default'){
-            value.prop('checked', 'checked');
-        }
-    });
+function refreshReports(){
+    getReports();
+    setTimeout(outputReports, 300);
+}
+function timeStamp(){
+    var date    = new Date();
+    var year    = date.getFullYear();
+    var month   = date.getMonth().pad();
+    var day     = date.getDay().pad();
+    var hours   = date.getHours().pad();
+    var minutes = date.getMinutes().pad();
+    var seconds = date.getSeconds().pad();
+
+    return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+}
+function outputReports(){
+    $("#reportsContent").empty();
+    if (! jQuery.isEmptyObject(reports.keys()) ){
+        var output = "";
+        var allReports = reports.keys();
+        jQuery.each(allReports, function(key, value){
+            var report = reports.get(value);
+            output = output.concat("<p>" + value + ": " + report + "</p>");
+        });
+        $("#reportsContent").append(output);
+    }   
 }
