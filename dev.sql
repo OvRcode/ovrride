f698d8372948393703cb6b3ac00de7050d7db23c
Mike Barnard <michael.d.barnard@gmail.com>
2015-01-22 15:01:25 -0500

    removed mandrill key from another place

--- dev.sql
+++ dev.sql
@@ -11,5 +11,4 @@ UPDATE wp_postmeta SET meta_value = replace(meta_value,'https://ovrride.com', 'h
 UPDATE wp_options SET option_value = replace(option_value, 'http://ovrride.com', 'http://local.ovrride.com') WHERE option_name = 'home' OR option_name = 'siteurl' OR option_name = 'dashboard_widget_options';
 UPDATE wp_options SET option_value = replace(option_value, 'https://ovrride.com', 'http://local.ovrride.com') WHERE option_name = 'home' OR option_name = 'siteurl' OR option_name = 'dashboard_widget_options';
 
-
-UPDATE wp_options SET option_value = replace(option_value, 'ABkKB4T19IsJKn_UprmSdw', 'SN74mu3IxIEYJz18FP31ag') WHERE option_name = 'wpmandrill';
+UPDATE wp_options SET option_value = 'a:7:{s:7:"api_key";s:22:"SN74mu3IxIEYJz18FP31ag";s:9:"from_name";s:8:"OvR Ride";s:13:"from_username";s:16:"info@ovrride.com";s:8:"reply_to";s:16:"info@ovrride.com";s:10:"trackopens";s:1:"1";s:11:"trackclicks";s:1:"1";s:4:"tags";s:0:"";}' WHERE option_name = 'wpmandrill';
