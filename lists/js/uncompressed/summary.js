69b3e6b054cac254518c870ae73e9d8f58a953d7
Mike Barnard <michael.d.barnard@gmail.com>
2015-01-30 13:52:13 -0500

    Increased reliability of reports loading -removed timer on loading -setup reports to append after data has loaded

--- lists/js/uncompressed/summary.js
+++ lists/js/uncompressed/summary.js
@@ -2473,7 +2473,9 @@ function getReports(){
         jQuery.each(parsed, function(key,value){
             reports.set(key, value);
         });
-    }).done(function(){})
+    }).done(function(){
+      outputReports();
+    })
     .fail(function(){ /* fail function here*/});
 }
 function getTripData(){
