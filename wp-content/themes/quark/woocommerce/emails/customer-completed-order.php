48e6916f8945e1f7642b83f4480681beab365575
Mike Barnard <michael.d.barnard@gmail.com>
2015-02-06 11:59:11 -0500

    Added pickup location directions to completed email Added condition for waiver button Moved Waiver button up to top of email

--- wp-content/themes/quark/woocommerce/emails/customer-completed-order.php
+++ wp-content/themes/quark/woocommerce/emails/customer-completed-order.php
@@ -7,13 +7,74 @@
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly 
+$pickups["Skate Brooklyn"]["url"] = "https://www.google.com/maps/place/Skate+Brooklyn/@40.68163,-73.979789,15z/";
+$pickups["Skate Brooklyn"]["instructions"] = "corner of St. Marks & 4th Ave.";
+$pickups["Union Square"]["url"] = "https://www.google.com/maps/place/41+Union+Square+W,+New+York,+NY+10003/";
+$pickups["Union Square"]["instructions"] = "NW side of USQ Park";
+$pickups["Homage Brooklyn"]["url"] = "https://www.google.com/maps/place/64+Bergen+St,+Brooklyn,+NY+11201/@40.6867357,-73.9911531,17z/";
+$pickups["Homage Brooklyn"]["instructions"] = "corner of Bergen & Smith";
+$pickups["Blades West"]["url"] = "https://www.google.com/maps/place/156+W+72nd+St,+New+York,+NY+10023/";
+$pickups["Blades West"]["instructions"] = "between Broadway & Columbus";
+$pickups["Blades Downtown"]["url"] = "https://www.google.com/maps/place/659+Broadway,+New+York,+NY+10012/";
+$pickups["Blades Downtown"]["instructions"] = "between Bleeker & 3rd St.";
+$pickups["Burton NYC"]["url"] = "https://www.google.com/maps/place/106+Spring+St,+New+York,+NY+10012/";
+$pickups["Burton NYC"]["instructions"] = "between Green & Mercer";
+$pickups["Upper Eastside"]["url"] = "https://www.google.com/maps/place/150+E+86th+St,+New+York,+NY+10028/";
+$pickups["Upper Eastside"]["instructions"] = "corner of 86th & Lexington Ave.";
+$pickups["REI Soho"]["url"] = "https://www.google.com/maps/place/303+Lafayette+St,+New+York,+NY+10012/";
+$pickups["REI Soho"]["instructions"] = "On Lafayette South of Houston";
+$pickups["Wburg, BK Union Pool"]["url"] = "https://www.google.com/maps/place/484+Union+Ave,+Brooklyn,+NY+11211/";
+$pickups["Wburg, BK Union Pool"]["instructions"] = "Corner of Union & Rodney";
+$pickups["Aegir Boardworks"]["url"] = "https://www.google.com/maps/place/Aegir+Boardworks/@40.703339,-73.990287,15z/";
+$pickups["Aegir Boardworks"]["instructions"] = "corner of Water St & Main St.";
+$pickups["Bedford Stuyvesant"]["url"] = "https://www.google.com/maps/place/54+MacDonough+St,+Brooklyn,+NY+11216/";
+$pickups["Bedford Stuyvesant"]["instructions"] = "btwn Marcy & Tompkins";
+$pickups["The Cliffs at LIC"]["url"] = "https://www.google.com/maps/place/11-11+44th+Dr,+Long+Island+City,+NY+11101/";
+$pickups["The Cliffs at LIC"]["instructions"] = "corner of 44th Drive & 11th St";
+$pickups["NJ Burton Store Menlo Park Mall"]["url"] = "https://www.google.com/maps/place/55+Parsonage+Rd,+Menlo+Park+Mall,+Edison,+NJ+08837/";
+$pickups["NJ Burton Store Menlo Park Mall"]["instructions"] = "Cheesecake Factory parking lot of the Menlo Park Mall";
+$pickups["Hoboken"]["url"] = "https://www.google.com/maps/place/CVS+Pharmacy+-+Photo/@40.736511,-74.030911,15z/";
+$pickups["Hoboken"]["instructions"] = "CVS on the Corner of Washington & Newark";
+$pickups["REI Paramus"]["url"] = "https://www.google.com/maps/place/REI/@40.914131,-74.055459,15z/";
+$pickups["REI Paramus"]["instructions"] = "On Forest Ave. Off RT 4, next to Lowes";
+$pickups["Freehold Raceway Mall"]["url"] = "https://www.google.com/maps/place/3681+U.S.+9,+Freehold,+NJ+07728/";
+$pickups["Freehold Raceway Mall"]["instructions"] = "In far parking lot facing Dick’s Sporting Goods";
+$pickups["Mount Everest"]["url"] = "https://www.google.com/maps/place/Mount+Everest/@40.991212,-74.034272,15z/";
+$pickups["Mount Everest"]["instructions"] = "corner of Washington & 3rd St";
+?>
 
 <?php do_action('woocommerce_email_header', $email_heading); ?>
 
 <p><?php printf( __( "Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) ); ?></p>
+        <?php if ( preg_match("/Package/",$order->email_order_items_table( true, false, true )) ): ?>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td>
+                    <div>
+                        <!--[if mso]>
+                            <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://litmus.com" style="height:36px;v-text-anchor:middle;width:150px;" arcsize="5%" strokecolor="#EB7035" fillcolor="#EB7035">
+                            <w:anchorlock/>
+                            <center style="color:#ffffff;font-family:Helvetica, Arial,sans-serif;font-size:16px;">I am a button &rarr;</center>
+                            </v:roundrect>
+                        <![endif]-->
+                        <a href="<?php echo get_site_url();?>/OvR%202014-15WAIVER.pdf.zip" style="background-color:#2BC9F1;border:1px solid ##2BC9F1;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;">Download Waiver</a>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    <p>To make things faster on the day of your trip, please download and print out a copy of our waiver. Bring a filled out and signed copy to your trip leader.</p>
+    <?php endif; ?>
+<?php 
+// Pickup Matching
+if (preg_match("/Pickup Location:.(.*).-/", $order->email_order_items_table( true, false, true ), $match)){
+    if ( isset($pickups[$match[1]]) ){
+        echo "<h2>Pickup Location: " . $match[1] . "</h2>";
+        echo "<p>" . $pickups[$match[1]]["instructions"] . "<br><a href='" . $pickups[$match[1]]["url"] ."'>Directions</a></p>";
+    }
+}
 
-<?php do_action('woocommerce_email_before_order_table', $order, false); ?>
+do_action('woocommerce_email_before_order_table', $order, false); ?>
 
 <h2><?php echo __( 'Order:', 'woocommerce' ) . ' ' . $order->get_order_number(); ?></h2>
 
@@ -27,6 +88,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 	</thead>
 	<tbody>
 		<?php echo $order->email_order_items_table( true, false, true ); ?>
+        <?php error_log($order->email_order_items_table( true, false, true )); ?>
 	</tbody>
 	<tfoot>
 		<?php
@@ -58,21 +120,5 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 <?php endif; ?>
 
 <?php woocommerce_get_template('emails/email-addresses.php', array( 'order' => $order )); ?>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-            <td>
-                <div>
-                    <!--[if mso]>
-                        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://litmus.com" style="height:36px;v-text-anchor:middle;width:150px;" arcsize="5%" strokecolor="#EB7035" fillcolor="#EB7035">
-                        <w:anchorlock/>
-                        <center style="color:#ffffff;font-family:Helvetica, Arial,sans-serif;font-size:16px;">I am a button &rarr;</center>
-                        </v:roundrect>
-                    <![endif]-->
-                    <a href="<?php echo get_site_url();?>/OvR%202014-15WAIVER.pdf.zip" style="background-color:#2BC9F1;border:1px solid ##2BC9F1;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;">Download Waiver</a>
-                </div>
-            </td>
-        </tr>
-    </table>
-<p>To make things faster on the day of your trip, please download and print out a copy of our waiver. Bring a filled out and signed copy to your trip leader.</p>
 
 <?php do_action('woocommerce_email_footer'); ?>
