0539f44685178540634c6190a35380750bb4d96f
Mike Barnard <michael.d.barnard@gmail.com>
2015-02-24 09:43:39 -0500

    Added back balance due status, closes #74

--- wp-content/plugins/ovr_functions/ovr_functions.php
+++ wp-content/plugins/ovr_functions/ovr_functions.php
@@ -89,6 +89,36 @@ function add_no_show_to_order_statuses( $order_statuses ) {
 }
 add_filter( 'wc_order_statuses', 'add_no_show_to_order_statuses' );
 
+// Add Balance Due Status
+function register_balance_due_order_status() {
+    register_post_status( 'wc-balance-due', array(
+        'label'                     => 'Balance Due',
+        'public'                    => true,
+        'exclude_from_search'       => false,
+        'show_in_admin_all_list'    => true,
+        'show_in_admin_status_list' => true,
+        'label_count'               => _n_noop( 'Balance Due <span class="count">(%s)</span>', 'Balance Due<span class="count">(%s)</span>' )
+    ) );
+}
+add_action( 'init', 'register_balance_due_order_status' );
+
+function add_balance_due_to_order_statuses( $order_statuses ) {
+  
+    $new_order_statuses = array();
+  
+    // add new order status after processing
+    foreach ( $order_statuses as $key => $status ) {
+  
+        $new_order_statuses[ $key ] = $status;
+  
+        if ( 'wc-processing' === $key ) {
+            $new_order_statuses['wc-balance-due'] = 'Balance Due';
+        }
+    }
+  
+    return $new_order_statuses;
+}
+add_filter( 'wc_order_statuses', 'add_balance_due_to_order_statuses' );
 //Remove Reviews tab from woocommerce
 function woocommerce_remove_reviews($tabs){
     unset( $tabs['reviews'] );
