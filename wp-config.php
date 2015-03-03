851502156a659b50ae0247ed8581d87cfc848a0a
Mike Barnard <michael.d.barnard@gmail.com>
2015-03-03 16:25:01 -0500

    changed wp salts to envvars, updated dev env with it's own keys

--- wp-config.php
+++ wp-config.php
@@ -54,14 +54,14 @@ define('DISABLE_WP_CRON', true);
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '***REMOVED***');
-define('SECURE_AUTH_KEY',  '***REMOVED***');
-define('LOGGED_IN_KEY',    '***REMOVED***');
-define('NONCE_KEY',        '***REMOVED***');
-define('AUTH_SALT',        '***REMOVED***');
-define('SECURE_AUTH_SALT', '***REMOVED***');
-define('LOGGED_IN_SALT',   '***REMOVED***');
-define('NONCE_SALT',       '***REMOVED***');
+define('AUTH_KEY',         getenv('WP_AUTH_KEY') );
+define('SECURE_AUTH_KEY',  getenv('WP_SECURE_AUTH_KEY') );
+define('LOGGED_IN_KEY',    getenv('WP_LOGGED_IN_KEY') );
+define('NONCE_KEY',        getenv('WP_NONCE_KEY') );
+define('AUTH_SALT',        getenv('WP_AUTH_SALT') );
+define('SECURE_AUTH_SALT', getenv('WP_SECURE_AUTH_SALT') );
+define('LOGGED_IN_SALT',   getenv('WP_LOGGED_IN_SALT') );
+define('NONCE_SALT',       getenv('WP_NONCE_SALT') );
 
 /**#@-*/
 
