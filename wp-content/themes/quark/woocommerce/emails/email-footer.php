e7374ab031d42600c7977c0768dccd00ccb00585
Mike Barnard <michael.d.barnard@gmail.com>
2015-03-17 16:54:44 -0400

    updated footer template

--- wp-content/themes/quark/woocommerce/emails/email-footer.php
+++ wp-content/themes/quark/woocommerce/emails/email-footer.php
@@ -4,30 +4,13 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates/Emails
- * @version     2.0.0
+ * @version     2.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
-// Load colours
-$base = get_option( 'woocommerce_email_base_color' );
-
-$base_lighter_40 = woocommerce_hex_lighter( $base, 40 );
-
-// For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline.
-$template_footer = "
-	border-top:0;
-	-webkit-border-radius:6px;
-";
-
-$credit = "
-	border:0;
-	color: $base_lighter_40;
-	font-family: Arial;
-	font-size:12px;
-	line-height:125%;
-	text-align:center;
-";
 ?>
 															</div>
 														</td>
@@ -43,35 +26,12 @@ $credit = "
                         	<tr>
                             	<td align="center" valign="top">
                                     <!-- Footer -->
-                                	<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer" style="<?php echo $template_footer; ?>">
+                                	<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
                                     	<tr>
                                         	<td valign="top">
                                                 <table border="0" cellpadding="10" cellspacing="0" width="100%">
                                                     <tr>
-                                                        <td colspan="2" valign="middle" style="text-align:center;">
-                                                            <a href="https://www.facebook.com/ovrride">
-                                                            <img src="http://ovrride.com/wp-content/themes/quark/images/soc-icons/facebook-square.png">
-                                                            </a>
-                                                            <a href="http://instagram.com/ovrride/">
-                                                                <img src="http://ovrride.com/wp-content/themes/quark/images/soc-icons/instagram-square.png">
-                                                            </a>
-                                                            <a href="https://twitter.com/ovrride">
-                                                                <img src="http://ovrride.com/wp-content/themes/quark/images/soc-icons/twitter-square.png">
-                                                            </a>
-                                                            <a href="https://plus.google.com/+OvRRide/">
-                                                                <img src="http://ovrride.com/wp-content/themes/quark/images/soc-icons/google-plus-square.png">
-                                                            </a>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td colspan="2" valign="middle" id="policy" style="<?php echo $credit; ?>">
-                                                            <a href="http://ovrride.com/terms-and-conditions/" style="<?php echo $credit; ?>">
-                                                                Cancellation Policy/Terms and Conditions
-                                                            </a>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td colspan="2" valign="middle" id="credit" style="<?php echo $credit; ?>">
+                                                        <td colspan="2" valign="middle" id="credit">
                                                         	<?php echo wpautop( wp_kses_post( wptexturize( apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) ) ) ) ); ?>
                                                         </td>
                                                     </tr>
