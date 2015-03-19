4a573235b1897825b1967708301f68cc7741777a
Mike Barnard <michael.d.barnard@gmail.com>
2015-03-19 14:45:39 -0400

    Updated font-awesome cs calls

--- wp-content/themes/ovr/404.php
+++ wp-content/themes/ovr/404.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 no-results not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><i class="icon-thumbs-down icon-large"></i> <?php _e( '&nbsp; Four Oh Fizzle, Oh Dip! <br> Now that wasn&rsquo;t supposed to happen!', 'quark' ); ?></h1>
+					<h1 class="entry-title"><i class="fa fa-thumbs-o-down fa-lg"></i> <?php _e( '&nbsp; Four Oh Fizzle, Oh Dip! <br> Now that wasn&rsquo;t supposed to happen!', 'quark' ); ?></h1>
 				</header>
 				<div class="entry-content clearfix">
 					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Maybe searching can help.', 'quark' ); ?></p>
