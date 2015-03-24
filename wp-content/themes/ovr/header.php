0889bdf0701f2f4e0d96381716c1982b215640bd
Mike Barnard <michael.d.barnard@gmail.com>
2015-03-24 18:30:03 -0400

    Removed chrome=1, ie chrome frame was retired last year

--- wp-content/themes/ovr/header.php
+++ wp-content/themes/ovr/header.php
@@ -18,8 +18,8 @@
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<!-- Always force latest IE rendering engine (even in intranet)-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta http-equiv="cleartype" content="on">
