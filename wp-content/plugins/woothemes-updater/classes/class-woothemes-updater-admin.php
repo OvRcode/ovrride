90c0b09be8d47107b58d07b2d901cdbfb26f8e7d
Mike Barnard <michael.d.barnard@gmail.com>
2015-05-05 08:43:54 -0400

    Updated woothemes updater to v1.5.5

--- wp-content/plugins/woothemes-updater/classes/class-woothemes-updater-admin.php
+++ wp-content/plugins/woothemes-updater/classes/class-woothemes-updater-admin.php
@@ -214,7 +214,7 @@ if ( jQuery( 'form[name="upgrade-themes"]' ).length ) {
 
 		add_action( 'load-' . $this->hook, array( $this, 'process_request' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-		add_action( 'admin_print_scripts-' . $this->hook, array( $this, 'enqueue_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 	} // End register_settings_screen()
 
 	/**
@@ -301,7 +301,7 @@ if ( jQuery( 'form[name="upgrade-themes"]' ).length ) {
 		echo '<img src="' . esc_url( $this->assets_url . 'images/getting-started.png' ) . '" alt="' . __( 'Getting Started', 'woothemes-updater' ) . '" />' . "\n";
 		echo '<h4>' . __( 'Getting Started', 'woothemes-updater' ) . '</h4>' . "\n";
 		echo '<ul>' . $this->_generate_link_list( $links ) . "\n";
-		echo '<li><em><a href="' . esc_url( 'https://twitter.com/WooSupport/' ) . '" title="' . esc_attr__( 'Follow the WooThemes Support Twitter', 'woothemes-updater' ) . '">' . __( 'Follow the WooThemes Support Twitter', 'woothemes-updater' ) . '</a></em></li>' . "\n";
+		echo '<li><em><a href="' . esc_url( 'https://twitter.com/WooThemes/' ) . '" title="' . esc_attr__( 'Follow WooThemes on Twitter', 'woothemes-updater' ) . '">' . __( 'Follow WooThemes on Twitter', 'woothemes-updater' ) . '</a></em></li>' . "\n";
 		echo '</ul>' . "\n";
 	} // End display_general_links()
 
@@ -425,13 +425,13 @@ if ( jQuery( 'form[name="upgrade-themes"]' ).length ) {
 	 * @since   1.2.0
 	 * @return  void
 	 */
-	public function enqueue_scripts () {
+	public function enqueue_scripts ( $hook ) {
 		$screen = get_current_screen();
 		wp_enqueue_script( 'post' );
 		wp_register_script( 'woothemes-updater-admin', $this->assets_url . 'js/admin.js', array( 'jquery' ) );
 
 		// Only load script and localization on helper admin page.
-		if ( 'dashboard_page_woothemes-helper' == $screen->id ) {
+		if ( in_array( $screen->id, array( 'dashboard_page_woothemes-helper' ) ) ) {
 			wp_enqueue_script( 'woothemes-updater-admin' );
 			$localization = array(
 				'ajax_url' => admin_url( 'admin-ajax.php' ),
@@ -454,7 +454,7 @@ if ( jQuery( 'form[name="upgrade-themes"]' ).length ) {
 
 		$action = $this->get_post_or_get_action( $supported_actions );
 
-		if ( $action && in_array( $action, $supported_actions ) && check_admin_referer( 'bulk-' . 'licenses' ) ) {
+		if ( $action && in_array( $action, $supported_actions ) && check_admin_referer( 'wt-helper-activate-license', 'wt-helper-nonce' ) ) {
 			$response = false;
 			$status = 'false';
 			$type = $action;
