7268b7d6c999068b1f6170193b8b16b2ad52ab5c
=Mike Barnard <michael.d.barnard@gmail.com>
2015-08-21 14:04:21 -0400

    Fixed error in SSL key generation

--- chef/certCheck.sh
+++ chef/certCheck.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 function generate_key {
-  openssl genrsa -des3 -passout pass:x -out /vagrant/local.ovrride.com.pass.key 2048
+  echo "Generating key"
   
-  openssl rsa -passin pass:x -in /vagrant/local.ovrride.com.pass.key -out /vagrant/local.ovrride.com.key
+  openssl genrsa -des3 -passout pass:x -out local.ovrride.com.pass.key 2048
+  
+  openssl rsa -passin pass:x -in local.ovrride.com.pass.key -out local.ovrride.com.key
 
-  rm /vagrant/local.ovrride.com.pass.key
+  rm local.ovrride.com.pass.key
 }
 
 function generate_signing_request {
-  openssl req -new -out /vagrant/local.ovrride.com.csr -key /vagrant/local.ovrride.com.key -config /vagrant/chef/openssl.cnf
-  #openssl req -new -key /vagrant/local.ovrride.com.key -out /vagrant/local.ovrride.com.csr -subj "/C=US/ST=NY/L=NYC/O=*.local.ovrride.com/OU=IT/CN=OvRride/emailAddress=devops@ovrride.com"
+  echo "Generating CSR"
+  openssl req -new -config chef/openssl.cnf -out local.ovrride.com.csr
 }
 
 function generate_certificate {
-  openssl x509 -req -days 3650 -in /vagrant/local.ovrride.com.csr -signkey /vagrant/local.ovrride.com.key -out /vagrant/local.ovrride.com.crt-extensions v3_req -extfile /vagrant/chef/openssl.cnf
-  #openssl x509 -req -days 365 -in /vagrant/local.ovrride.com.csr -signkey /vagrant/local.ovrride.com.key -out /vagrant/local.ovrride.com.crt
+  echo "Generating CERT"
+  openssl x509 -req -days 3650 -in local.ovrride.com.csr -signkey local.ovrride.com.key -out local.ovrride.com.crt -extensions v3_req -extfile chef/openssl.cnf
 }
 
+# GET DIR OF THIS SCRIPT
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# Move up one level
+cd $DIR; cd ../
+
 # Verify key,csr,crt are present
-if [ ! -f /vagrant/local.ovrride.com.key ]; then
+if [ ! -f local.ovrride.com.key ]; then
   generate_key
   generate_signing_request
   generate_certificate
-elif [ ! -f /vagrant/local.ovrride.com.csr ]; then
+elif [ ! -f local.ovrride.com.csr ]; then
   generate_signing_request
   generate_certificate
-elif [ ! -f /vagrant/local.ovrride.com.crt ]; then
+elif [ ! -f local.ovrride.com.crt ]; then
   generate_certificate
 else
   echo "All Files present"
 fi
 
 # Verify key and crt match
-crt="$(openssl x509 -noout -modulus -in /vagrant/local.ovrride.com.crt | openssl md5)"
-key="$(openssl rsa -noout -modulus -in /vagrant/local.ovrride.com.key | openssl md5)"
+crt="$(openssl x509 -noout -modulus -in local.ovrride.com.crt | openssl md5)"
+key="$(openssl rsa -noout -modulus -in local.ovrride.com.key | openssl md5)"
+
 if [ "$crt" = "$key" ]; then
   echo "Key and Cert match"
 else
