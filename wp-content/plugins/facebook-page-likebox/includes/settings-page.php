8256e93d69757613c496c64cd4c32ea0abfe0a80
Mike Barnard <michael.d.barnard@gmail.com>
2015-08-25 11:00:35 -0400

    Added new Facebook plugin
    old plugin was generating a lot of errors in console

---
+++ wp-content/plugins/facebook-page-likebox/includes/settings-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author https://smashr.org
+ * Plugin: Facebook Page Plugin
+ */
+?>
+
+    <div class="wrap">
+
+        <form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+            <input type="hidden" name="info_update" id="info_update" value="true"/>
+
+            <?php wp_nonce_field('fmz-update-setting','fmz-update-setting'); ?>
+
+            <u><h2>Facebook Page Plugin by Smashr.org</h2></u>
+
+            <div id="poststuff" class="metabox-holder has-right-sidebar">
+
+
+                <div style="float:left;width:70%;">
+
+                    <br>
+
+                    <?php
+                    require_once 'setting-page/crunchlr-left-column.php';
+                    require_once 'setting-page/crunchlr-right-column.php';
+                    require_once 'setting-page/crunchlr-footer.php';
+
+                    ?>
