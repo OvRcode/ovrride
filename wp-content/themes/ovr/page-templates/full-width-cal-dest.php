8cb302ff50dc69c082b61c3598c533433eebeba0
Mike Barnard <michael.d.barnard@gmail.com>
2015-08-31 11:45:36 -0400

    Converted destination resort icons to sprites

--- wp-content/themes/ovr/page-templates/full-width-cal-dest.php
+++ wp-content/themes/ovr/page-templates/full-width-cal-dest.php
@@ -29,47 +29,47 @@ get_header(); ?>
 
                 <h3 id="squelch-taas-header-0" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-0" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-0">New York</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-0 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-0" aria-labelledby="squelch-taas-header-0" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/hunter-mt/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo  $img_path;?>/images/destination-imgs/hunter.png">Hunter MT</a><br>
-                <a href="/destinations/windham-mt/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/windham.png">Windham MT</a>
+                <a href="/destinations/hunter-mt/"><div class="location hunter"></div>Hunter MT</a><br>
+                <a href="/destinations/windham-mt/"><div class="location windham"></div>Windham MT</a>
                 </div>
 
                 <h3 id="squelch-taas-header-1" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-1" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-1">Pennsylvania</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-1 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-1" aria-labelledby="squelch-taas-header-1" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/camelback/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/camelback.png">Camelback</a>
+                <a href="/destinations/camelback/"><div class="location camelback"></div>Camelback</a>
                 </div>
 
                 <h3 id="squelch-taas-header-2" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-2" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-2">Vermont</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-2 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-2" aria-labelledby="squelch-taas-header-2" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/stratton/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/stratton.png">Stratton</a><br>
-                <a href="/destinations/mount-snow/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/mount-snow.png">Mount Snow</a><br>
-                <a href="/destinations/killington/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/killington.png">Killington</a><br>
-                <a href="/destinations/stowe/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/stowe.png">Stowe</a><br>
-                <a href="/destinations/sugarbush/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/sugarbush.png">Sugarbush</a>
+                <a href="/destinations/stratton/"><div class="location stratton"></div>Stratton</a><br>
+                <a href="/destinations/mount-snow/"><div class="location mount-snow"></div>Mount Snow</a><br>
+                <a href="/destinations/killington/"><div class="location killington"></div>Killington</a><br>
+                <a href="/destinations/stowe/"><div class="location stowe"></div>Stowe</a><br>
+                <a href="/destinations/sugarbush/"><div class="location sugarbush"></div>Sugarbush</a>
                 </div>
                 
                 <h3 id="squelch-taas-header-3" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-3" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-3">Out West</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-3 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-3" aria-labelledby="squelch-taas-header-3" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/lake-tahoe/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/tahoe.png">Lake Tahoe</a><br>
-                <a href="/destinations/breckenridge/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/breck.png">Breckenridge</a><br>
-                <a href="/destinations/snowbird/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/snowbird.png">Snowbird</a>
+                <a href="/destinations/lake-tahoe/"><div class="location tahoe"></div>Lake Tahoe</a><br>
+                <a href="/destinations/breckenridge/"><div class="location breckenridge"></div>Breckenridge</a><br>
+                <a href="/destinations/snowbird/"><div class="location snowbird"></div>Snowbird</a>
                 </div>
 
                 <h3 id="squelch-taas-header-4" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-4" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-4">South America</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-4 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-4" aria-labelledby="squelch-taas-header-4" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/north-chile/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/chile.png">North Chile</a><br>
-                <a href="/destinations/south-chile/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/chile.png">South Chile</a><br>
-                <a href="/destinations/north-argentina/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/argentina.png">North Argentina</a><br>
-                <a href="/destinations/south-argentina/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/argentina.png">South Argentina</a>
+                <a href="/destinations/north-chile/"><div class="location chile"></div>North Chile</a><br>
+                <a href="/destinations/south-chile/"><div class="location chile"></div>South Chile</a><br>
+                <a href="/destinations/north-argentina/"><div class="location argentina"></div>North Argentina</a><br>
+                <a href="/destinations/south-argentina/"><div class="location argentina"></div>South Argentina</a>
                 </div>
                 
                 <h3 id="squelch-taas-header-5" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-5" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-5">Canada</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-5 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-5" aria-labelledby="squelch-taas-header-5" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/whistler/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/whistler.png">Whistler</a>
+                <a href="/destinations/whistler/"><div class="location whistler"></div>Whistler</a>
                 </div>
 
                 <h3 id="squelch-taas-header-6" class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all ui-accordion-icons" role="tab" aria-controls="ui-accordion-squelch-taas-accordion-0-panel-6" aria-selected="false" tabindex="-1"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span><a href="#squelch-taas-accordion-shortcode-content-6">Japan</a></h3>
                 <div class="squelch-taas-accordion-shortcode-content-6 ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" style="display: none; " id="ui-accordion-squelch-taas-accordion-0-panel-6" aria-labelledby="squelch-taas-header-6" role="tabpanel" aria-expanded="false" aria-hidden="true">
-                <a href="/destinations/japan/"><img style="float: left; margin: 0px 4px 0px 0px;" alt="" src="<?php echo $img_path;?>/images/destination-imgs/japan-icon.png">Japan</a>
+                <a href="/destinations/japan/"><div class="location japan"></div>Japan</a>
                 </div>
                 </div>
             </div>
