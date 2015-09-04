f381ba375246a071d304e067b2f0d73437bb40d6
Mike Barnard <michael.d.barnard@gmail.com>
2015-09-03 21:23:04 -0400

    Fixed style on editors for includes and rates

--- wp-content/plugins/woocommerce-trips/includes/admin/views/html-trip-includes.php
+++ wp-content/plugins/woocommerce-trips/includes/admin/views/html-trip-includes.php
@@ -10,9 +10,6 @@ $editorSettings = array('textarea_rows' => 12);
     <div class="options_group">
         <h1>Includes</h1>
         <p>Content for includes tab on product page</p>
-        <?php 
-        remove_filter('the_content', 'do_shortcode',11 );
-        wp_editor( $content , "_wc_trip_includes", $editorSettings);
-        ?>
+        <?php wp_editor( $content , "_wc_trip_includes", $editorSettings); ?>
     </div>
 </div>
