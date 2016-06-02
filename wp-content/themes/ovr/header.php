ee597501432ce7159af6b31e7ebaf54f4ff7f4f8
Mike Barnard <michael.d.barnard@gmail.com>
2016-06-02 16:33:26 -0400

    Added facebook sdk js to remove facebook plugin

--- wp-content/themes/ovr/header.php
+++ wp-content/themes/ovr/header.php
@@ -74,6 +74,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 
 <div class="visuallyhidden"><a href="#primary" title="<?php esc_attr_e( 'Skip to main content', 'quark' ); ?>"><?php _e( 'Skip to main content', 'quark' ); ?></a></div>
 
