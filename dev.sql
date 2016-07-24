ec73f955106b677661cfc8dcb8147b64a408a77f
Mike Barnard <michael.d.barnard@gmail.com>
2016-07-24 13:22:51 -0400

    fixed typo in serialized array value for dev env

--- dev.sql
+++ dev.sql
@@ -11,7 +11,7 @@ UPDATE wp_postmeta SET meta_value = replace(meta_value,'https://ovrride.com', 'h
 UPDATE wp_options SET option_value = replace(option_value, 'http://ovrride.com', 'http://local.ovrride.com') WHERE option_name = 'home' OR option_name = 'siteurl' OR option_name = 'dashboard_widget_options';
 UPDATE wp_options SET option_value = replace(option_value, 'https://ovrride.com', 'https://local.ovrride.com') WHERE option_name = 'home' OR option_name = 'siteurl' OR option_name = 'dashboard_widget_options';
 
-UPDATE wp_options SET option_value = ' a:5:{s:10:"from_email";s:16:"info@ovrride.com";s:9:"from_name";s:7:"OvRride";s:16:"enable_sparkpost";b:0;s:14:"sending_method";s:3:"api";s:15:"enable_tracking";b:1;'WHERE option_name = 'sp_settings' LIMIT 1;
+UPDATE wp_options SET option_value = ' a:5:{s:10:"from_email";s:16:"info@ovrride.com";s:9:"from_name";s:7:"OvRride";s:16:"enable_sparkpost";b:0;s:14:"sending_method";s:3:"api";s:15:"enable_tracking";b:1;}'WHERE option_name = 'sp_settings' LIMIT 1;
 
 UPDATE mysql.user SET Host = '%' WHERE User = 'root' AND Host = 'localhost';
 DELETE From mysql.user WHERE User = 'root' AND Host <> '%';
