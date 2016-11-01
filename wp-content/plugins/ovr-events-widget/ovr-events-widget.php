d3d0c23a446f4ea3127ee2ffb8d176704f607777
Mike Barnard <michael.d.barnard@gmail.com>
2016-11-01 16:36:55 -0400

    Added link to event title. Trimmed dates and days of the week off titles. added stock notifications and color indicators for stock.

--- wp-content/plugins/ovr-events-widget/ovr-events-widget.php
+++ wp-content/plugins/ovr-events-widget/ovr-events-widget.php
@@ -24,29 +24,41 @@ class ovr_events_widget extends WP_Widget {
 public function form($instance) {
   // Widget Admin Form
   $eventsID = $this->get_field_id( 'events' );
-  $menu_orderID = $this->get_field_id( 'menu_order' );
-  $eventsLabel = 'Number of Events to list(1-30):';
-  $menu_orderLabel = 'Override Date Sorting with menu order field on products:';
+  $menuOrderID = $this->get_field_id( 'menu_order' );
+  $seatCountID = $this->get_field_id( 'seat_count' );
+  $eventsLabel = 'Number of events to list(1-30):';
+  $menuOrderLabel = 'Override date dorting with menu order field on products:';
+  $seatCountLabel = 'Number of seats left to initiate countdown (1-52):';
   $eventsFieldName = $this->get_field_name('events');
-  $menu_orderFieldName = $this->get_field_name('menu_order');
+  $menuOrderFieldName = $this->get_field_name('menu_order');
+  $seatCountFieldName = $this->get_field_name('seat_count');
   if ( isset($instance['events']) ) {
     $events = esc_attr($instance['events']);
   } else {
     $events = 10;
   }
   if ( isset($instance['menu_order']) ) {
-    $menu_order = checked;
+    $menuOrder = checked;
   } else {
-    $menu_order = "";
+    $menuOrder = "";
+  }
+  if ( isset($instance['seat_count']) ) {
+    $seatCount = esc_attr($instance['seat_count']);
+  } else {
+    $seatCount = 30;
   }
   echo <<<ADMINFORM
   <p>
   <label for="{$eventsID}">{$eventsLabel}</label>
-  <input id="{$eventsID}" name="{$eventsFieldName}" type="number" min="1" max="30" value="{$events}" />
+  <input id="{$eventsID}" name="{$eventsFieldName}" type="number" min="1" max="30" value="{$events}">
   </p>
   <p>
-  <label for="{$menu_orderID}">{$menu_orderLabel}</label>
-  <input id="{$menu_orderID}" name="{$menu_orderFieldName}" type="checkbox" value="true" {$menu_order}>
+  <label for="{$menuOrderID}">{$menuOrderLabel}</label>
+  <input id="{$menuOrderID}" name="{$menuOrderFieldName}" type="checkbox" value="true" {$menuOrder}>
+  </p>
+  <p>
+  <label for="{$seatCountID}">{$seatCountLabel}</label>
+  <input id="{$seatCount}" name="{$seatCountFieldName}" type="number" min="1" max="52" value="{$seatCount}">
   </p>
 ADMINFORM;
 }
@@ -54,12 +66,30 @@ public function update( $new_instance, $old_instance ) {
   $instance = array();
   $instance['events'] = ( ! empty( $new_instance['events'] ) ) ? strip_tags( $new_instance['events'] ) : '';
   $instance['menu_order'] = ( ! empty( $new_instance['menu_order'] ) ) ? strip_tags( $new_instance['menu_order'] ) : '';
+  $instance['seat_count'] = ( ! empty( $new_instance['seat_count'] ) ) ? strip_tags( $new_instance['seat_count'] ) : '';
   return $instance;
 }
 public function widget( $args, $instance ) {
+  wp_enqueue_style( 'ovr_event_widget_style', plugin_dir_url( __FILE__ ) . 'ovr-event-widget.css');
   $trip = $this->returnTrips($instance['events'], $instance['menu_order']);
   foreach($trip as $id => $data ) {
-    echo $data->post_title ."<br />";
+
+    if ( strcmp($data->stock_status, "outofstock") === 0 ) {
+      $label = "<span class='soldOut'>SOLD OUT</span>";
+    } else if ( $data->stock <= intval($instance['seat_count']) ) {
+      $label = "<span class='spots'>" . $data->stock . " Spots Remaining</span>";
+    } else {
+      $label = "<span class='available'>AVAILABLE</span>";
+    }
+
+    echo <<<WIDGETHTML
+    <div class="event_block">
+    <a href="{$data->guid}">{$data->post_title}</a><br />
+    {$data->dateLabel}<br />
+    {$label}
+    </p>
+WIDGETHTML;
+
   }
 }
 function returnTrips($numberOfTrips, $menu_order){
@@ -81,7 +111,8 @@ function returnTrips($numberOfTrips, $menu_order){
       "SELECT `post_id`,
       MAX(CASE WHEN `wp_postmeta`.`meta_key` = '_stock' THEN `wp_postmeta`.`meta_value` END) as 'stock',
       MAX(CASE WHEN `wp_postmeta`.`meta_key` = '_stock_status' THEN `wp_postmeta`.`meta_value` END) as 'stock_status',
-      MAX(CASE WHEN `wp_postmeta`.`meta_key` = '_wc_trip_start_date' THEN `wp_postmeta`.`meta_value` END) as 'date'
+      MAX(CASE WHEN `wp_postmeta`.`meta_key` = '_wc_trip_start_date' THEN `wp_postmeta`.`meta_value` END) as 'date',
+      MAX(CASE WHEN `wp_postmeta`.`meta_key` = '_wc_trip_end_date' THEN `wp_postmeta`.`meta_value` END) as 'end_date'
       FROM `wp_postmeta`
       WHERE `post_id` = '%d'", intval($id)
     );
@@ -90,6 +121,16 @@ function returnTrips($numberOfTrips, $menu_order){
     $trip[$id]->stock = $meta[$id]->stock;
     $trip[$id]->stock_status = $meta[$id]->stock_status;
     $trip[$id]->date = $meta[$id]->date;
+
+    if ( strtotime($meta[$id]->end_date) > strtotime($meta[$id]->date) ) {
+      $trip[$id]->dateLabel = date('F jS - ', strtotime($trip[$id]->date)) . date('jS, Y', strtotime($meta[$id]->end_date));
+    } else {
+      $trip[$id]->dateLabel = date('F jS, Y', strtotime($trip[$id]->date));
+    }
+    // Remove Dates from title
+    $trip[$id]->post_title = preg_replace('/[JFMASOND][aepuco][nbrylgptvc][.eyt].*/','',$trip[$id]->post_title);
+    // Remove any leftover day abbreviations in titile
+    $trip[$id]->post_title = preg_replace('/[STMWF][uhaoer][neutdi][rs.][.$]/','',trim($trip[$id]->post_title));
   }
   // Sort trips by trip date
   usort($trip, function($a, $b){
