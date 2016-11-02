9520435f1e8ce678e582dae8e4bc15f8a9085415
Mike Barnard <michael.d.barnard@gmail.com>
2016-11-02 17:30:55 -0400

    Finished OvR Jumbotron Widget Admin settings.

--- wp-content/plugins/ovr-jumbotron-widget/ovr-jumbotron-widget.php
+++ wp-content/plugins/ovr-jumbotron-widget/ovr-jumbotron-widget.php
@@ -21,8 +21,60 @@ class ovr_jumbotron_widget extends WP_Widget {
     array( 'description' => __( 'Widget to display a large feature story', 'ovr_jumbotron_domain' ), )
     );
   }
+  public function form($instance) {
+    // Widget Admin Form
+    // Get list of recent posts
+    $args = array(
+      'numberposts' => 20,
+      'offset' => 0,
+      'category' => 0,
+      'orderby' => 'post_date',
+      'order' => 'DESC',
+      'include' => '',
+      'exclude' => '',
+      'meta_key' => '',
+      'meta_value' =>'',
+      'post_type' => 'post',
+      'post_status' => 'publish',
+      'suppress_filters' => true
+    );
+    $recent_posts = wp_get_recent_posts( $args, ARRAY_A);
+    $options = "<option>Select a post for top story</option>";
+    # Make sure post value set in instance array is set, could possibly not be in the last 20 posts
+    $instanceCheck = false;
+    foreach($recent_posts as $index => $post_data ) {
+      if ( $instance['post'] == $post_data['ID'] ) {
+        $instanceCheck = true; // Only change if we found the value
+        $selected = "selected";
+      } else {
+        $selected = "";
+      }
+      $options .= "<option value='{$post_data['ID']}' {$selected}>{$post_data['post_title']}</option>";
+    }
+    unset($recent_posts);
+    if ( !$instanceCheck ) {
+      $options .= "<option value='{$instance['post']}' selected>". get_the_title($instance['post']) ."</option>";
+    }
+    // Setup field id and name
+    $postID = $this->get_field_id( 'post' );
+    $postLabel = 'Selected post:';
+    $postFieldName = $this->get_field_name('post');
+    echo <<<ADMINFORM
+    <p>
+    <label for="{$postID}">{$postLabel}</label>
+    <select id="{$postID}" name="{$postFieldName}" style="width:100%;">
+      {$options}
+    </select>
+    </p>
+ADMINFORM;
+  }
+  public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['post'] = ( ! empty( $new_instance['post'] ) ) ? strip_tags( $new_instance['post'] ) : '';
+    return $instance;
+  }
   public function widget( $args, $instance ) {
-    echo "Jumbotron!";
+    echo $instance['post'];
   }
 }
 function ovr_jumbotron_load_widget() {
