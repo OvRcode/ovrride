23c44ceca279c3080f243f7ff330da675adc820e
Mike Barnard <michael.d.barnard@gmail.com>
2016-11-04 18:20:30 -0400

    Fixed featured video inner background alignment.

--- wp-content/plugins/ovr-featured-video-widget/ovr-featured-video-widget.js
+++ wp-content/plugins/ovr-featured-video-widget/ovr-featured-video-widget.js
@@ -5,7 +5,7 @@ jQuery(function($) {
     var container = $(".ovr_featured_video_box");
     var title = $(".ovr_featured_video_inner h4");
     var newWidth = container.width() * 0.98;
-    var newHeight = (container.height() - 20) * 0.98;
+    var newHeight = (container.height() - 20) * 0.90;
     video.removeAttr('height').removeAttr('width');
     video.height(newHeight).width(newWidth);
   }
