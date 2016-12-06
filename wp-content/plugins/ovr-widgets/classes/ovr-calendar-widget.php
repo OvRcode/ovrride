0ec57c45510403861f7e99a9f8b7f20a52678a80
Mike Barnard <michael.d.barnard@gmail.com>
2016-12-06 14:28:38 -0500

    Added pointer property for clickable calendar areas. Minified and enqueued style for calendar

--- wp-content/plugins/ovr-widgets/classes/ovr-calendar-widget.php
+++ wp-content/plugins/ovr-widgets/classes/ovr-calendar-widget.php
@@ -128,7 +128,7 @@ class ovr_calendar_widget extends WP_Widget {
     wp_enqueue_script( 'jquery_spin_js', plugin_dir_url( dirname(__FILE__) ) . 'js/jquery.spin.js', array('jquery','spin_js'), false, true);
     wp_enqueue_script( 'spin_js', plugin_dir_url( dirname(__FILE__) ) . 'js/spin.min.js');
     wp_enqueue_script( 'ovr_calendar_js', plugin_dir_url( dirname(__FILE__) ) . 'js/ovr-calendar-widget.js', array('jquery.webui-popover-js', 'jquery_spin_js'), false, true);
-    wp_enqueue_style('ovr_calendar_style', plugin_dir_url( dirname(__FILE__) ) . 'css/ovr-calendar-widget.css');
+    wp_enqueue_style('ovr_calendar_style', plugin_dir_url( dirname(__FILE__) ) . 'css/ovr-calendar-widget.min.css');
     //$nonce = wp_create_nonce('ovr_calendar');
     $nonced_url = wp_nonce_url( admin_url( 'admin-ajax.php'), 'ovr_calendar', 'ovr_calendar_shift' );
     wp_localize_script('ovr_calendar_js', 'ovr_calendar_vars', array( 'ajax_url' => $nonced_url ) );
