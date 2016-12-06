bdca691be9263fd2ca5af82a352e0c8a33995af6
Mike Barnard <michael.d.barnard@gmail.com>
2016-12-06 16:17:40 -0500

    removed link from top of calendar

--- wp-content/plugins/ovr-widgets/classes/ovr-calendar-widget.php
+++ wp-content/plugins/ovr-widgets/classes/ovr-calendar-widget.php
@@ -129,7 +129,7 @@ class ovr_calendar_widget extends WP_Widget {
     wp_enqueue_script( 'spin_js', plugin_dir_url( dirname(__FILE__) ) . 'js/spin.min.js');
     wp_enqueue_script( 'ovr_calendar_js', plugin_dir_url( dirname(__FILE__) ) . 'js/ovr-calendar-widget.js', array('jquery.webui-popover-js', 'jquery_spin_js'), false, true);
     wp_enqueue_style('ovr_calendar_style', plugin_dir_url( dirname(__FILE__) ) . 'css/ovr-calendar-widget.min.css');
-    
+
     $nonced_url = wp_nonce_url( admin_url( 'admin-ajax.php'), 'ovr_calendar', 'ovr_calendar_shift' );
     wp_localize_script('ovr_calendar_js', 'ovr_calendar_vars', array( 'ajax_url' => $nonced_url ) );
 
@@ -143,7 +143,7 @@ class ovr_calendar_widget extends WP_Widget {
                 <li class="prev"><i class="fa fa-arrow-left fa-lg" aria-hidden="true"></i></li>
                 <li class="next"><i class="fa fa-arrow-right fa-lg" aria-hidden="true"></i></li>
                 <li>
-                  <a href="/calendar"><h4 class="month_year">{$month_year}</h4></a>
+                  <h4 class="month_year">{$month_year}</h4>
                 </li>
               </ul>
             </div>
