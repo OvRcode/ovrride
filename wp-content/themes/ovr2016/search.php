a20b9ce733fb62ed238ed66b1f72c10fa46c8eb6
Mike Barnard <michael.d.barnard@gmail.com>
2017-01-01 10:32:22 -0500

    Added formatting to blog search page

--- wp-content/themes/ovr2016/search.php
+++ wp-content/themes/ovr2016/search.php
@@ -8,7 +8,9 @@
 get_header(); ?>
 
 	<?php if ( have_posts() ) : ?>
-
+		<div class="container-fluid">
+			<div class="col-md-10 col-md-offset-1 col-sm-12 mainBackground">
+				<div class="col-sm-9 pull-right">
 		<header>
 			<h2 class="page-title"><?php printf( __( 'Search Results for: %s', '_tk' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
 		</header><!-- .page-header -->
@@ -27,6 +29,10 @@ get_header(); ?>
 		<?php get_template_part( 'no-results', 'search' ); ?>
 
 	<?php endif; // end of loop. ?>
-
+	</div>
+	<div class="col-sm-3 push-left">
 <?php get_sidebar(); ?>
+</div>
+</div>
+</div>
 <?php get_footer(); ?>
