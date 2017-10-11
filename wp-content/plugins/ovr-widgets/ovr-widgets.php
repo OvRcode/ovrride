2edc59a96b3e98ceae96354605bba0a2f9a6fb86
Mike Barnard <michael.d.barnard@gmail.com>
2017-10-11 17:06:27 -0400

    Added placeholder admin page for adding events. Added custom table to wpdb

--- wp-content/plugins/ovr-widgets/ovr-widgets.php
+++ wp-content/plugins/ovr-widgets/ovr-widgets.php
@@ -4,7 +4,7 @@
 * Description:  Collection of widgets for the ovrride.com site
 * Author: Mike Barnard
 * Author URI: http://github.com/barnardm
-* Version: 1.2.2
+* Version: 1.3.0
 * License: MIT License
 */
 require('classes/ovr-blog-feature-widget.php');
@@ -20,6 +20,9 @@ require('classes/ovr-trip-feature-widget.php');
 require('classes/ovr-calendar-widget.php');
 
 function ovr_load_widgets() {
+
+	setup_calendar_table();
+
 	register_widget( 'ovr_blog_feature_widget' );
 	register_widget( 'ovr_contact_widget' );
 	register_widget( 'ovr_dual_trip_feature_widget' );
@@ -33,4 +36,29 @@ function ovr_load_widgets() {
 	register_widget( 'ovr_calendar_widget' );
 }
 
+function setup_calendar_table() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "ovr_calendar_custom_events";
+
+	$sql = "CREATE TABLE $table_name (
+  id mediumint(9) NOT NULL AUTO_INCREMENT,
+  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  label tinytext NOT NULL,
+  status text NOT NULL,
+  url varchar(55) DEFAULT '' NOT NULL,
+  PRIMARY KEY  (id)
+) $charset_collate;";
+
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+dbDelta( $sql );
+}
+function ovr_widgets_admin_setup_menu() {
+	add_menu_page( 'OvRride Calendar Events', 'Calendar Events', 'manage_options', 'ovr-calendar-widget-menu', 'ovr_calendar_events', 'dashicons-calendar-alt', 59 );
+}
+
+function ovr_calendar_events() {
+	echo "<h1>Is this thing on?</h1>";
+}
 add_action( 'widgets_init', 'ovr_load_widgets' );
+add_action('admin_menu', 'ovr_widgets_admin_setup_menu');
