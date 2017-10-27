c99ba6d775aaacb9bf6146b19fb17b30deca428c
Mike Barnard <michael.d.barnard@gmail.com>
2017-10-27 10:30:34 -0400

    Added function to ovr2016 functions to parallelize host names

--- wp-content/themes/ovr2016/functions.php
+++ wp-content/themes/ovr2016/functions.php
@@ -544,3 +544,17 @@ function custom_woocommerce_product_add_to_cart_text() {
 	}
 
 }
+
+/* Parallelize host names */
+function parallelize_hostnames($url, $id) {
+$hostname = par_get_hostname($url); //call supplemental function
+$url = str_replace(parse_url(get_bloginfo('url'), PHP_URL_HOST), $hostname, $url);
+return $url;
+}
+function par_get_hostname($name) {
+$subdomains = array('static1.ovrride.com','static2.ovrride.com'); //add your subdomains here, as many as you want.
+$host = abs(crc32(basename($name)) % count($subdomains));
+$hostname = $subdomains[$host];
+return $hostname;
+}
+add_filter('wp_get_attachment_url', 'parallelize_hostnames', 10, 2);
