2f7b7ad4e1fe6121eea94e994853bad62ff656b8
Mike Barnard <michael.d.barnard@gmail.com>
2018-04-23 11:52:00 -0400

    Removed files for old development environment. New environment is in a seprate repo

--- cookbooks/gzip/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: gzip
-# Recipe:: default
-# License:: Apache 2.0 (see http://www.apache.org/licenses/LICENSE-2.0)
-#
-package 'gzip'
--- site-cookbooks/mysqld/metadata.rb
+++
@@ -1,2 +0,0 @@
-name 'mysqld'
-depends 'mysqld'
