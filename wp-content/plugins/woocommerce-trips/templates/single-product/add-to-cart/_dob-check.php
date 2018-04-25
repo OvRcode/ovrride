8699d1926ffe4c7b2cd6f35f325a1e484df5f01f
Mike Barnard <michael.d.barnard@gmail.com>
2018-04-25 14:31:00 -0400

    Added logic to disable add to cart button if guest is not 21 when 21 is selected as age limit

--- wp-content/plugins/woocommerce-trips/templates/single-product/add-to-cart/_dob-check.php
+++ wp-content/plugins/woocommerce-trips/templates/single-product/add-to-cart/_dob-check.php
@@ -1,12 +1,17 @@
 
 <div class="dobCheck">
-  <label for="wc_trip_age_check"><strong>Is this guest at least 18 years of age?</strong><span class="required"> *</span></label>
+  <input type="hidden" value="<?php echo $age_limit; ?>" id="wc_trip_age_limit"></input>
+  <label for="wc_trip_age_check"><strong>Is this guest at least <?php echo $age_limit; ?> years of age?</strong><span class="required"> *</span></label>
   <br />
   <input type="radio" id="wc_trip_age_check" name="wc_trip_age_check" value="yes" data-required="true"> Yes
   <br />
   <input type="radio" id="wc_trip_age_check" name="wc_trip_age_check" value="no" data-required="true"> No
   <br />
-  <p>Guests under 18 years of age are welcome to join us as long as they abide by the terms of the <a href="http://ovrride.com/ovrride-age-policy/" target="_blank"><strong>OvRride Age Policy</strong></a>, and we are aware of the underage guest.</p>
+  <?php if ( "21" == $age_limit ): ?>
+    <p>For this trip guests need to be at least 21 years of age, we are unable to accomodate guests under 21 years of age.</p>
+  <?php elseif ( "18" == $age_limit) : ?>
+    <p>Guests under 18 years of age are welcome to join us as long as they abide by the terms of the <a href="http://ovrride.com/ovrride-age-policy/" target="_blank"><strong>OvRride Age Policy</strong></a>, and we are aware of the underage guest.</p>
+  <?php endif;?>
   <label class="dobLabel"><strong>Date of Birth</strong><span class="required">*</span></label>
   <div class="dob clearfix">
     <div class="monthGroup">
