b6177e98dfd932d26d561d766f871eb1281dc5cf
Mike Barnard <michael.d.barnard@gmail.com>
2018-05-16 10:04:24 -0400

    removed pingdom scripts/settings. no longer using this service.

--- wp-content/themes/ovr2016/includes/js/pingdom.js
+++
@@ -1,9 +0,0 @@
-var _prum = [['id', ovr.pingdom_id],
-             ['mark', 'firstbyte', (new Date()).getTime()]];
-(function() {
-    var s = document.getElementsByTagName('script')[0]
-      , p = document.createElement('script');
-    p.async = 'async';
-    p.src = '//rum-static.pingdom.net/prum.min.js';
-    s.parentNode.insertBefore(p, s);
-})();
