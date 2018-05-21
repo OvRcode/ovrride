7d010a24452b9def3bfbd0ef57ef8b0667f68a62
Mike Barnard <michael.d.barnard@gmail.com>
2018-05-21 13:43:37 -0400

    updated messages reguarding age limits on booking form

--- wp-content/plugins/woocommerce-trips/templates/single-product/add-to-cart/_dob-check.php
+++ wp-content/plugins/woocommerce-trips/templates/single-product/add-to-cart/_dob-check.php
@@ -7,10 +7,12 @@
   <br />
   <input type="radio" id="wc_trip_age_check" name="wc_trip_age_check" value="no" data-required="true"> No
   <br />
-  <?php if ( "21" == $age_limit ): ?>
+<?php if ( "18" == $age_limit) : ?>
+  <p>Guests under 18 years of age are welcome to join us as long as they abide by the terms of the <a href="http://ovrride.com/ovrride-age-policy/" target="_blank"><strong>OvRride Age Policy</strong></a>, and we are aware of the underage guest.</p>
+<?php elseif ( "21" == $age_limit ): ?>
+    <p>Guests under 21 years of age are welcome to join us as long as they abide by the terms of the <a href="http://ovrride.com/ovrride-age-policy/" target="_blank">OvRride Age Policy</a>, and we are aware of the underage guest.</p>
+  <?php elseif ( "21+" == $age_limit ): ?>
     <p>For this trip guests need to be at least 21 years of age, we are unable to accomodate guests under 21 years of age.</p>
-  <?php elseif ( "18" == $age_limit) : ?>
-    <p>Guests under 18 years of age are welcome to join us as long as they abide by the terms of the <a href="http://ovrride.com/ovrride-age-policy/" target="_blank"><strong>OvRride Age Policy</strong></a>, and we are aware of the underage guest.</p>
   <?php endif;?>
   <label class="dobLabel"><strong>Date of Birth</strong><span class="required">*</span></label>
   <div class="dob clearfix">
