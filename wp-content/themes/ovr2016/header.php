035740ee11acf98ebe0ac6793490ff481b1648c2
Anthony L Rivera <anthonyr@complex.com>
2020-01-25 17:52:57 -0500

    add Google Tag Manager code

--- wp-content/themes/ovr2016/header.php
+++ wp-content/themes/ovr2016/header.php
@@ -47,21 +47,37 @@ if (  is_front_page() || is_archive() || is_home() ) {
 
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-MXZNP92');</script>
+	<!-- End Google Tag Manager -->
+	
 	<!-- Event snippet for Booked conversion page -->
-<!-- Global site tag (gtag.js) - Google AdWords: 828851634 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-828851634"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+	<!-- Global site tag (gtag.js) - Google AdWords: 828851634 -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-828851634"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
 
-  gtag('config', 'AW-828851634');
-	gtag('event', 'conversion', {'send_to': 'AW-828851634/W6_DCIf6oHoQsoudiwM'});
-</script>
+	  gtag('config', 'AW-828851634');
+	  gtag('event', 'conversion', {'send_to': 'AW-828851634/W6_DCIf6oHoQsoudiwM'});
+	</script>
+	
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+	
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXZNP92"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
 	<?php do_action( 'before' ); ?>
 
 <nav class="site-navigation">
