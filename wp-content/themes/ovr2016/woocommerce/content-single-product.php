0250c8b25482168d431e12ca977169d0562def68
Ada Lambrecht <ada@ovrride.com>
2023-05-20 09:47:02 -0400

    updated theme woocommerce templates

--- wp-content/themes/ovr2016/woocommerce/content-single-product.php
+++ wp-content/themes/ovr2016/woocommerce/content-single-product.php
@@ -10,30 +10,26 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+/**
+ * Hook: woocommerce_before_single_product.
+ *
+ * @hooked woocommerce_output_all_notices - 10
+ */
+do_action( 'woocommerce_before_single_product' );
+
+if ( post_password_required() ) {
+	echo get_the_password_form(); // WPCS: XSS ok.
+	return;
 }
-
-?>
-
-<?php
-	/**
-	 * woocommerce_before_single_product hook.
-	 *
-	 * @hooked wc_print_notices - 10
-	 */
-	 //do_action( 'woocommerce_before_single_product' );
-
-	 if ( post_password_required() ) {
-	 	echo get_the_password_form();
-	 	return;
-	 }
 ?>
 <div class="col-sm-12 col-md-10 col-md-offset-1 mainBackground">
 	<?php woocommerce_show_messages(); ?>
@@ -41,52 +37,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div class="row">
 			<div class="col-sm-12">
 	<?php
-		woocommerce_breadcrumb();
-		/**
-		 * woocommerce_before_single_product_summary hook.
-		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
-		 */
-		do_action( 'woocommerce_before_single_product_summary' );
+				woocommerce_breadcrumb();
+	/**
+	 * Hook: woocommerce_before_single_product_summary.
+	 *
+	 * @hooked woocommerce_show_product_sale_flash - 10
+	 * @hooked woocommerce_show_product_images - 20
+	 */
+	do_action( 'woocommerce_before_single_product_summary' );
 	?>
 	</div>
 </div>
 <div class="col-sm-12 col-md-6">
-
-		<div class="summary entry-summary">
-
+	<div class="summary entry-summary">
 		<?php
-			/**
-			 * woocommerce_single_product_summary hook.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
-
-		</div><!-- .summary -->
-	</div>
-
-	<?php
 		/**
-		 * woocommerce_after_single_product_summary hook.
+		 * Hook: woocommerce_single_product_summary.
 		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_upsell_display - 15
-		 * @hooked woocommerce_output_related_products - 20
+		 * @hooked woocommerce_template_single_title - 5
+		 * @hooked woocommerce_template_single_rating - 10
+		 * @hooked woocommerce_template_single_price - 10
+		 * @hooked woocommerce_template_single_excerpt - 20
+		 * @hooked woocommerce_template_single_add_to_cart - 30
+		 * @hooked woocommerce_template_single_meta - 40
+		 * @hooked woocommerce_template_single_sharing - 50
+		 * @hooked WC_Structured_Data::generate_product_data() - 60
 		 */
-		do_action( 'woocommerce_after_single_product_summary' );
+		do_action( 'woocommerce_single_product_summary' );
+		?>
+	</div>
+</div>
+	<?php
+	/**
+	 * Hook: woocommerce_after_single_product_summary.
+	 *
+	 * @hooked woocommerce_output_product_data_tabs - 10
+	 * @hooked woocommerce_upsell_display - 15
+	 * @hooked woocommerce_output_related_products - 20
+	 */
+	do_action( 'woocommerce_after_single_product_summary' );
 	?>
-
-		<meta itemprop="url" content="<?php the_permalink(); ?>" />
-
-	</div><!-- #product-<?php the_ID(); ?> -->
+	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+</div>
 </div>
 <?php do_action( 'woocommerce_after_single_product' ); ?>
