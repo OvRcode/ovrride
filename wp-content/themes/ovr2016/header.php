c860e5ac806940ba6d882720b146073751ab0b0b
Ada Lambrecht <ada@ovrride.com>
2023-06-20 18:28:24 -0400

    fixed warning

--- wp-content/themes/ovr2016/header.php
+++ wp-content/themes/ovr2016/header.php
@@ -9,10 +9,12 @@
 global $post;
 
 $title = wp_title( '|', false, 'right' );
-if ( has_post_thumbnail( $post->ID ) || ! is_woocommerce()) {
-	$ogImage = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' );
-} else {
-	$ogImage = get_site_url( null, 'wp-content/themes/ovr2016/includes/images/ovr_og.png');
+if ( isset($post) && isset($post->ID)) {
+	if ( has_post_thumbnail( $post->ID ) || ! is_woocommerce()) {
+		$ogImage = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'thumbnail' );
+	} else {
+		$ogImage = get_site_url( null, 'wp-content/themes/ovr2016/includes/images/ovr_og.png');
+	}
 }
 if (  is_front_page() || is_archive() || is_home() ) {
 	$ogDescription = esc_attr(wp_strip_all_tags(get_option("about_ovr", "OvRride"), true));
