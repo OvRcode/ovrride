c055a4e739e8744d67a35cff9342b35ebdabee18
Ada Lambrecht <ada@ovrride.com>
2023-06-30 11:44:24 -0400

    Fixed author/urls

--- wp-content/plugins/woocommerce-customer-note-email/woocommerce-customer-note-email.php
+++ wp-content/plugins/woocommerce-customer-note-email/woocommerce-customer-note-email.php
@@ -3,8 +3,8 @@
    * Plugin Name: WooCommerce Customer Note Email
    * TODO:ADD URI
    * Description: Plugin to add an extra email when a customer adds a note to an order
-   * Author: Mike Barnard
-   * Version: 0.2
+   * Author: Ada Lambrecht
+   * Version: 0.2.1
    *
    * License: GNU General Public License v3.0
    * License URI: http://www.gnu.org/licenses/gpl-3.0.html
