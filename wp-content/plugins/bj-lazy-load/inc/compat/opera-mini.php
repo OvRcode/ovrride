bccf8fc713f879c5ef21296e80d02944bc41a6fc
Ada Lambrecht <ada@ovrride.com>
2023-10-16 21:45:58 -0400

    Removed bj-lazy-load

--- wp-content/plugins/bj-lazy-load/inc/compat/opera-mini.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function bjll_compat_operamini() {
-	if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && false !== strpos( $_SERVER['HTTP_USER_AGENT'], 'Opera Mini' ) ) {
-		add_filter( 'bjll/enabled', '__return_false' );
-	}
-}
-
-add_action( 'bjll/compat', 'bjll_compat_operamini' );
